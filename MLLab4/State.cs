5e6fa409a1172643c5c472abc1822f597bd735a0
Dennis Lund <denniz_lund@hotmail.com>
2019-12-10 12:51:39 +0100

    Added state class

---
+++ MLLab4/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLLab4
+{
+    public class State
+    {
+        public State()
+        {
+
+        }
+    }
+
+
+}
