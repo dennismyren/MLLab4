57059077031716082c1d76190945d6dc21b1d5a5
Tim Nyman <36240398+TimNNyman@users.noreply.github.com>
2019-12-10 12:50:18 +0100

    Added loading of the file

--- MLLab4/Program.cs
+++ MLLab4/Program.cs
@@ -7,6 +7,18 @@ namespace MLLab4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine(RetrieveData()[0]);
+        }
+
+        private string[] RetrieveData()
+        {
+            string[] raw = System.IO.File.ReadAllLines(@"..\..\..\city 1.txt");
+            return raw;
         }
     }
 }
